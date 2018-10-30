25dab42cd1ea66471d9683df80eaff326702e00f
Armin Gerten <armin@gerten.de>
2018-10-30 11:25:32 +0100

    Checked in prototype for deviceCode auth.
    Signed-off-by: Armin Gerten <armin@gerten.de>

--- IoTHub.py
+++ IoTHub.py
@@ -7,26 +7,51 @@ class IoTHub:
     def __init__ (self, gateId, endpoint, verifySslCertificate):
         self.gateId = gateId
         self.endpoint = endpoint
-        self.baseUrl = endpoint + "/iot/v1"
+        self.baseUrl = "https://iothub-" + endpoint + "/iot/v1"
         self.verifySslCertificate = verifySslCertificate
 
         self.httpClient = requests.Session()
         self.httpClient.headers.update({'Accept' : 'application/json'})
         self.httpClient.headers.update({'Content-Type' : 'application/json'})
+        
+        self.httpAuthClient = requests.Session()
+        self.httpAuthClient.headers.update({'Accept' : 'application/json'})
+        self.httpAuthClient.headers.update({'Content-Type' : 'application/x-www-form-urlencoded'})
+        self.authUrl = "https://identity-" + endpoint
 
         requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
         print("IoTHub initialized")
 
-    def activate(self, activationCode):
-        print("IoTHub.activate({})".format(activationCode))
-        url = self.baseUrl + "/activationrequest"
-        payload = {'message' : {'activation_code' : activationCode , 'gate_id' : self.gateId }, 'timestamp' : self.getCurrentUTCTimestamp() }
+    def requestAuthorization(self):
+        print("IoTHub.requestAuthorization()")
+        url = self.authUrl + "/connect/deviceauthorization"
+        payload = 'client_id=axoom-device'
 
-        r = self.httpClient.post(url, data=json.dumps(payload), verify=self.verifySslCertificate)
+        r = self.httpAuthClient.post(url, data=payload, verify=self.verifySslCertificate)
+        print("IoTHub.requestAuthorization(..) returned {}".format(r.status_code))
+        
+        if r.status_code == 200:
+            self.deviceauthorization = r.json()
+            return true
+        
+        return false
 
-        print("IoTHub.activate(..) returned {}".format(r.status_code))
-
+    def requestToken(self):
+        print("IoTHub.requestToken()")
+        url = self.authUrl + "/connect/token"
+        payload = 'client_id=axoom-device&grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code&device_code=' + self.deviceauthorization['device_code']
+        
+        r = self.httpAuthClient.post(url, data=payload, verify=self.verifySslCertificate)
+        
+        if r.status_code == 200:
+            print("IoTHub.requestToken(..) returned 200")
+            self.httpClient.headers.update({'Authorization' : r.json()['token_type'] + ' ' + r.json()['access_token']})
+            return '';
+        else:
+            print("IoTHub.requestToken(..) returned {statusCode} with error: {error}".format(statusCode=r.status_code, error=r.json()['error']))
+            return error=r.json()['error'];
+        
     def heartbeat(self):
         print("IoTHub.heartbeat()")
         
--- PiGate.py
+++ PiGate.py
@@ -11,24 +11,25 @@ class PiGate:
     onlySendUpdateOnChange = False
 
     def __init__(self):
-        self.iothub = IoTHub("{generate GUID for gateId}", "https://iothub-{customerId}.myaxoom.com", False)
+        self.iothub = IoTHub("{generate GUID for gateId}", "{customerId}.myaxoom.com", False)
 
     def run(self):
-        self.iothub.activate("0123456789")
         GPIO.setmode(GPIO.BCM)
 
+        self.waitForActivation()
+        
         while True:
             try:
                 self.iothub.heartbeat()
 
-                self.observeDht(dht.DHT22, 6, "b38e7e44", "fe00895f")
-                self.observeDht(dht.DHT11, 5, "46bb3fa8", "9a7dbbe2")
-                self.observeGPIO(22, "412523d0") # Window 1 reed contact
-                self.observeGPIO(26, "633d8df6")  # Window 2 reed contact
-                self.observeGPIO(25, "aca899ea")  # Window 3 reed contact
-                self.observeGPIO(23, "40c1c680")  # Window 4 reed contact
-                self.observeGPIO(18, "c5ca96bd")  # Motion sensor 1
-                self.observeGPIO(24, "abb5ca5c")  # Motion sensor 2
+                #self.observeDht(dht.DHT22, 6, "b38e7e44", "fe00895f")
+                #self.observeDht(dht.DHT11, 5, "46bb3fa8", "9a7dbbe2")
+                #self.observeGPIO(22, "412523d0") # Window 1 reed contact
+                #self.observeGPIO(26, "633d8df6")  # Window 2 reed contact
+                #self.observeGPIO(25, "aca899ea")  # Window 3 reed contact
+                #self.observeGPIO(23, "40c1c680")  # Window 4 reed contact
+                #self.observeGPIO(18, "c5ca96bd")  # Motion sensor 1
+                #self.observeGPIO(24, "abb5ca5c")  # Motion sensor 2
 
             except Exception as ex:
                 print("Exception occurred!")
@@ -36,6 +37,16 @@ class PiGate:
     
             time.sleep(2)
 
+    def waitForActivation(self):
+        tokenStatus = 'AUTH_NOT_STARTED'
+        while (tokenStatus != ''):
+            tokenStatus = self.iothub.requestToken()
+            
+            if (tokenStatus == 'authorization_pending'):
+                time.sleep(6)
+            else:
+                self.iothub.requestAuthorization()
+            
     def observeDht(self, dhtType, gpioPin, dsIdHumidity, dsIdTemperature):
         h,t = dht.read_retry(dhtType, gpioPin)
         self.iothub.observe(dsIdHumidity, h)
